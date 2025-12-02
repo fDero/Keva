ab0f9c3e5db8c2efea648d6ea862c064aebd2809
fDero <francescodero@outlook.it>
2025-12-02 21:58:37 +0100

    Add the storage package

---
+++ storage/storage_commons.go
@@ -0,0 +1,28 @@
+package storage
+
+type StorageSettings struct {
+	internalStorage map[string]string
+}
+
+func NewStorageSettings() *StorageSettings {
+	return &StorageSettings{
+		internalStorage: make(map[string]string),
+	}
+}
+
+func (ss *StorageSettings) ProcessEvent(event_raw_text string) error {
+	return nil
+}
+
+func (ss *StorageSettings) DeleteRecord(key string) {
+	delete(ss.internalStorage, key)
+}
+
+func (ss *StorageSettings) UpsertRecord(key string, value string) {
+	ss.internalStorage[key] = value
+}
+
+func (ss *StorageSettings) FetchRecord(key string) (string, bool) {
+	value, present := ss.internalStorage[key]
+	return value, present
+}
